3391dc51b1d2863ef2518da19ce5c95d448840f6
harsh-not-haarsh <anand.2@iitj.ac.in>
2021-01-10 01:18:33 +0530

    beautified code

--- HTTPServer.py
+++ HTTPServer.py
@@ -1,46 +1,42 @@
 import socket
 from HTTPRequest import HTTPRequest
 
-FORMAT_STRING = "Please give operation in the correct form: num1<space>+<space>num2<space>/<space>num3.\n All other operations are invalid".encode('utf8')
+FORMAT_STRING = "Please give operation in the correct form: num1<space>+<space>num2<space>/<space>num3.\n All other operations are invalid".encode(
+    'utf8')
+
 
 class HTTPServer:
-    
+
     def __init__(self, host='127.0.0.1', port=8888):
         self.host = host
         self.port = port
 
-    status_codes = {
-        200: 'OK',
-        404: 'Not Found',
-    }
-
     def handle_single_connection(self, conn):
         while True:
             try:
-                data = conn.recv(1024) 
+                data = conn.recv(1024)
             except socket.error:
                 conn.close()
-                break               
+                break
             if data == b"" or data == b"quit":
                 conn.close()
-                break       
+                break
             response = self.handle_request(data)
             conn.sendall(response)
 
     def start(self):
         # To be defined in server files
         return
-                 
 
     def handle_request(self, data):
-        
+
         request = HTTPRequest(data)
 
         operations = request.method.split()
 
         n = len(operations)
 
-        if (n-1)%2 != 0:
+        if (n-1) % 2 != 0:
             return FORMAT_STRING
 
         try:
@@ -48,23 +44,21 @@ class HTTPServer:
         except:
             return FORMAT_STRING
 
-        i=0
+        i = 0
         try:
             while i < n-1:
                 if operations[i+1] == "+":
-                    ans +=int(operations[i+2])
+                    ans += int(operations[i+2])
                 elif operations[i+1] == "-":
-                    ans -=int(operations[i+2])
+                    ans -= int(operations[i+2])
                 elif operations[i+1] == "*":
-                    ans *=int(operations[i+2])
+                    ans *= int(operations[i+2])
                 elif operations[i+1] == "/":
-                    ans /=int(operations[i+2])
+                    ans /= int(operations[i+2])
                 else:
                     return FORMAT_STRING
-                i+=2
-        except :
+                i += 2
+        except:
             return FORMAT_STRING
 
         return str(ans).encode('utf8')
-    
-    
--- Server2.py
+++ Server2.py
@@ -1,13 +1,9 @@
 from HTTPServer import HTTPServer
-import sys
 import socket
 import threading
 
 
 class Server2(HTTPServer):
-
-    
-
 
     def start(self):
 
@@ -17,23 +13,24 @@ class Server2(HTTPServer):
         s.listen(5)
 
         print("Listening at", s.getsockname())
-        
+
         threads = []
 
         try:
             while True:
                 conn, addr = s.accept()
                 print("Connected by", addr)
-                t = threading.Thread(target=self.handle_single_connection, args=(conn,), daemon = True)
+                t = threading.Thread(
+                    target=self.handle_single_connection, args=(conn,), daemon=True)
                 t.start()
                 threads.append(conn)
-                
-                
+
         except KeyboardInterrupt:
             s.close()
             for conn in threads:
                 conn.close()
             quit()
+
 
 if __name__ == '__main__':
     server = Server2()
